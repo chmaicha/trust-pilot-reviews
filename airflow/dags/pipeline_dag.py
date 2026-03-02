bf01cfe2a6e12c0fe14556445c79c067873af534
aboulouafa <abl.chaimaie.2000@gmail.com>
2026-03-02 09:58:53 +0100

    translate to french

--- airflow/dags/pipeline_dag.py
+++ airflow/dags/pipeline_dag.py
@@ -23,7 +23,7 @@ with DAG(
         data=json.dumps({
             "url": "https://www.google.com/maps/place/Caf%C3%A9+de+Flore/@48.8541623,2.3300297,16z/data=!4m8!3m7!1s0x47e671d781fb9dab:0x18bba6dd45e173ff!8m2!3d48.8541588!4d2.3326046!9m1!1b1!16zL20vMDhkeXY4?entry=ttu&g_ep=EgoyMDI2MDIyMy4wIKXMDSoASAFQAw%3D%3D",
             "name": "cafe_de_flore",
-            "max_reviews": 100
+            "max_reviews": "20"
         }),
         headers={"Content-Type": "application/json"},
     )
